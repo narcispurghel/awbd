b96b4afb111146be325999e4c927751b449b5c48
Narcis Purghel <narcispurghel@gmail.com>
2026-05-10 12:05:19 +0300

    feat: add main Gradle project configuration

---
+++ src/main/java/com/github/narcispurghel/awdb/AwdbApplication.java
@@ -0,0 +1,12 @@
+package com.github.narcispurghel.awdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwdbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwdbApplication.class, args);
+    }
+}
---
+++ src/test/java/com/github/narcispurghel/awdb/AwdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.narcispurghel.awdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
