4f04f2bc77e54053003fa468a1abf5d496b83278
Narcis Purghel <narcispurghel@gmail.com>
2026-05-18 21:48:29 +0300

    feat(user-service): add UserJpaRepository

---
+++ user-service/src/main/java/com/github/narcispurghel/userservice/repository/UserJpaRepository.java
@@ -0,0 +1,9 @@
+package com.github.narcispurghel.userservice.repository;
+
+import com.github.narcispurghel.userservice.entity.User;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserJpaRepository extends JpaRepository<User, UUID> {}
